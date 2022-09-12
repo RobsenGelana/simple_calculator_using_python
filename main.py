25043f7d83a82a8066e413fbd46b0f972a9ba5cc
RobsenGelana <robsengelana21@gmail.com>
2022-09-12 19:54:57 +0300

    creating functions for the operations

---
+++ main.py
@@ -0,0 +1,23 @@
+from art import logo
+
+#print(logo)
+
+#creating a function for the operations
+
+#addition
+def add(number1, number2):
+    return number1 + number2
+
+#Subtraction
+def subtraction(number1, number2):
+    return number1 - number2
+
+
+#division
+def divide(number1, number2):
+    return number1 / number2
+
+#multiplication
+def multiply(number1, number2):
+    return number1 * number2
+
